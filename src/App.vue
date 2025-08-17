a683366987e160279d9acd65070709ffdc78536d
MrShahin2004 <hashmyshahin@gmail.com>
2025-08-17 10:48:24 +0330

    Must've wrapped the whole content in another "div".

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <template>
-  <div class="weather-section">
-    <input class="weather-input" placeholder="Insert a city or country" type="text" />
+  <div class="weather">
+    <div class="weather-section">
+      <input class="weather-input" placeholder="Insert a city or country" type="text" />
 
-    <div class="weather-headers">
-      <div class="weather-region">
-        <h2 class="region-city" style="color: black">City</h2>
-        <h2 class="region-country" style="color: black">Country</h2>
+      <div class="weather-headers">
+        <div class="weather-region">
+          <h2 class="region-city" style="color: black">City</h2>
+          <h2 class="region-country" style="color: black">Country</h2>
+        </div>
+
+        <h3 class="weather-date" style="color: black">Current Date</h3>
       </div>
 
-      <h3 class="weather-date" style="color: black">Current Date</h3>
-    </div>
+      <h1 class="weather-temp" style="color: black">Current Temp</h1>
+      <h2 class="weather-status" style="color: black">Weather Status</h2>
 
-    <h1 class="weather-temp" style="color: black">Current Temp</h1>
-    <h2 class="weather-status" style="color: black">Weather Status</h2>
-
-    <div class="weather-temps">
-      <h3 class="max-temp" style="color: black">Max Temp</h3>
-      <h3 class="min-temp" style="color: black">Min Temp</h3>
+      <div class="weather-temps">
+        <h3 class="max-temp" style="color: black">Max Temp</h3>
+        <h3 class="min-temp" style="color: black">Min Temp</h3>
+      </div>
     </div>
   </div>
 </template>
 
-<script></script>
+<script>
+</script>
 
 <style></style>
