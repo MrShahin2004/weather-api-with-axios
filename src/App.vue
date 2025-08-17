520721d5c21f73e6a4400a71b2c7621be5e5e5b0
MrShahin2004 <hashmyshahin@gmail.com>
2025-08-17 11:20:08 +0330

    HTML completed, styles will be changed later if it looks bad.

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,34 @@
 <template>
-  <div class="weather">
-    <div class="weather-section">
-      <input class="weather-input" placeholder="Insert a city or country" type="text" />
+  <div
+    class="weather w-[100%] h-[100%] flex flex-col items-center"
+    style="justify-content: space-evenly"
+  >
+    <div
+      class="weather-section w-[100%] h-[100%] flex flex-col items-center"
+      style="justify-content: space-evenly"
+    >
+      <input
+        class="weather-input w-[100%] rounded-[0.5rem] placeholder-black placeholder-opacity-100 focus:bg-[white]"
+        style="padding: 0.5rem 1rem"
+        placeholder="Insert a city or country"
+        type="text"
+      />
 
-      <div class="weather-headers">
-        <div class="weather-region">
-          <h2 class="region-city" style="color: black">City</h2>
-          <h2 class="region-country" style="color: black">Country</h2>
+      <div class="weather-headers flex flex-col justify-center items-center">
+        <div class="weather-region flex flex-col justify-center items-center">
+          <h2 class="region-city text-xl" style="color: black">City</h2>
+          <h2 class="region-country text-xl" style="color: black">Country</h2>
         </div>
 
-        <h3 class="weather-date" style="color: black">Current Date</h3>
+        <h3 class="weather-date text-xl" style="color: black">Current Date</h3>
       </div>
 
-      <h1 class="weather-temp" style="color: black">Current Temp</h1>
-      <h2 class="weather-status" style="color: black">Weather Status</h2>
+      <h1 class="weather-temp text-3xl" style="color: black">Current Temp</h1>
+      <h2 class="weather-status text-2xl" style="color: black">Weather Status</h2>
 
       <div class="weather-temps">
-        <h3 class="max-temp" style="color: black">Max Temp</h3>
-        <h3 class="min-temp" style="color: black">Min Temp</h3>
+        <h3 class="max-temp text-xl" style="color: black">Max Temp</h3>
+        <h3 class="min-temp text-xl" style="color: black">Min Temp</h3>
       </div>
     </div>
   </div>
